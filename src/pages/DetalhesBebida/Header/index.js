6e6fa881ab947e5ddf782c7162e5531c28a973df
Leticia <leticia.bora@gmail.com>
2020-11-19 09:32:13 -0300

    teste

--- src/pages/DetalhesBebida/Header/index.js
+++ src/pages/DetalhesBebida/Header/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import clipBoard from 'clipboard-copy';
 import favIcon from '../../../images/whiteHeartIcon.svg';
