e3eed430401ac4084ff39b946ebceb20d1bb080b
Leticia <leticia.bora@gmail.com>
2020-11-19 09:01:11 -0300

    teste

---
+++ src/pages/Explorar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.css';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+const Explorar = () => (
+  <div>
+    <Header hideSearch>Explorar</Header>
+    <Link to="/explorar/comidas" data-testid="explore-food" className="btn">Explorar Comidas</Link>
+    <Link to="/explorar/bebidas" data-testid="explore-drinks" className="btn">Explorar Bebidas</Link>
+    <Footer />
+  </div>
+);
+
+export default Explorar;
