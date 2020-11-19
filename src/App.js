99ed690dbdd3bd3886a9ddfdd8393a57a75c5163
Leticia <leticia.bora@gmail.com>
2020-11-19 09:56:14 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-
-import Provider from './context/AppProvider';
-
-import Rotas from './routes';
-
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Login from './Pages/TelaLogin';
+import TelaInicial from './Pages/TelaPrincipal';
+import DetalhesComidas from './Pages/TelaDetalhesComida';
+import DetalhesBebidas from './Pages/TelaDetalhesBebida';
+import ProcessoReceitas from './Pages/TelaReceitaProcesso';
+import Explorar from './Pages/TelaExplorar';
+import ExplorarBebidaComida from './Pages/TelaExplorarBebidaComida';
+import ExplorarIngredientes from './Pages/TelaExplorarIngredientes';
+import ExplorarComidasAreas from './Pages/TelaExplorarLocal';
+import Perfil from './Pages/TelaPerfil';
+import ReceitasFeitas from './Pages/TelaReceitaFeitas';
+import ReceitasFavoritas from './Pages/TelaReceitasFavoritas';
 
-const App = () => (
-  <div>
-    <Provider>
-      <BrowserRouter>
-        <Rotas />
-      </BrowserRouter>
-    </Provider>
-  </div>
-);
+function App() {
+  return (
+    <Switch>
+      <Route path="/comidas/:id_da_receita/in-progress" component={ProcessoReceitas} />
+      <Route path="/bebidas/:id_da_receita/in-progress" component={ProcessoReceitas} />
+      <Route path="/comidas/:id_da_receita" component={DetalhesComidas} />
+      <Route path="/bebidas/:id_da_receita" component={DetalhesBebidas} />
+      <Route exact path="/comidas" component={TelaInicial} />
+      <Route exact path="/bebidas" component={TelaInicial} />
+      <Route path="/explorar/comidas/ingredientes" component={ExplorarIngredientes} />
+      <Route path="/explorar/bebidas/ingredientes" component={ExplorarIngredientes} />
+      <Route path="/explorar/comidas/area" component={ExplorarComidasAreas} />
+      <Route path="/explorar/comidas" component={ExplorarBebidaComida} />
+      <Route path="/explorar/bebidas" component={ExplorarBebidaComida} />
+      <Route path="/explorar" component={Explorar} />
+      <Route path="/receitas-feitas" component={ReceitasFeitas} />
+      <Route path="/receitas-favoritas" component={ReceitasFavoritas} />
+      <Route path="/perfil" component={Perfil} />
+      <Route exact path="/" component={Login} />
+      {/* <Route render={() => <h1 className="naoEncontrada">Página não encontrada</h1>} /> */}
+    </Switch>
+  );
+}
+
 export default App;
