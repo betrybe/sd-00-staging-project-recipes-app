f0b210b3e02c4c4e78d0c277e75ed939b5eaed94
Leticia <leticia.bora@gmail.com>
2020-11-19 22:01:52 -0300

    teste 6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import RecipesProvider from './context/RecipesProvider';
-import Routers from './routers/index';
+// import React from 'react';
+// import { BrowserRouter } from 'react-router-dom';
+// import RecipesProvider from './context/RecipesProvider';
+// import Routers from './routers/index';
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <RecipesProvider>
-        <Routers />
-      </RecipesProvider>
-    </BrowserRouter>
-  );
-}
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <RecipesProvider>
+//         <Routers />
+//       </RecipesProvider>
+//     </BrowserRouter>
+//   );
+// }
