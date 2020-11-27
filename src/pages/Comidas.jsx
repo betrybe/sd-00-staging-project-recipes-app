8d27563efb5c7566b80444c96fd9c0c10530b490
Leticia <leticia.bora@gmail.com>
2020-11-27 17:27:33 -0300

    grupo 4

--- src/pages/Comidas.jsx
+++ src/pages/Comidas.jsx
@@ -16,7 +16,8 @@ export default function Comidas({ history }) {
     loading,
     meals,
     mealsByIngredient,
-    setMeals } = useContext(Context);
+    setMeals,
+    showMealsByIngredient } = useContext(Context);
   const [hidden, setHidden] = useState(true);
   const [selected] = useState(false);
 
@@ -95,7 +96,7 @@ export default function Comidas({ history }) {
       ))}
       {hidden ? '' : <SearchBar />}
       <h1>{ titulo }</h1>
-      {loading ? <p>Loading</p>
+      {loading || showMealsByIngredient ? <p>Loading</p>
         : meals.filter((meal, index) => meal && index < twelve)
           .map((meal, index) => (
             <button
@@ -119,7 +120,7 @@ export default function Comidas({ history }) {
     </div>
   );
 }
-
+// comentario para push;
 Comidas.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
