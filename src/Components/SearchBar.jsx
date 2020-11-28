63815f60e76aa2b7730dedda56e3ac836717d414
Leticia <leticia.bora@gmail.com>
2020-11-27 22:30:08 -0300

    ajuste export componente searchbar

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,70 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import Context from '../context/Context';
+import * as api from '../services/Api';
 
-export default function SearchBar() {
-  // const [searchParam, setSearchParam] = useState('');
+function SearchBar() {
+  const [searchParam, setSearchParam] = useState('');
   const [termoBusca, setTermoBusca] = useState('');
-  // const { setMeals, setDrinks, setLoading, titulo } = useContext(Context);
+  const { setMeals, setDrinks, setLoading, titulo } = useContext(Context);
 
-  // const handleChange = ({ target }) => {
-  //   setSearchParam(target.value);
-  // };
+  const handleChange = ({ target }) => {
+    setSearchParam(target.value);
+  };
 
-  // const buscaComidas = async () => {
-  //   if (searchParam === 'ingredient') {
-  //     setLoading(true);
-  //     const filteredMeals = await api.fetchFoodByIngredients(termoBusca);
-  //     setMeals(filteredMeals);
-  //     setLoading(false);
-  //   }
-  //   if (searchParam === 'name') {
-  //     setLoading(true);
-  //     const filteredMeals = await api.fetchFoodByName(termoBusca);
-  //     setMeals(filteredMeals);
-  //     setLoading(false);
-  //   }
-  //   if (termoBusca.length === 1 && searchParam === 'first-letter') {
-  //     setLoading(true);
-  //     const filteredMeals = await api.fetchFoodByFirstLetter(termoBusca);
-  //     setMeals(filteredMeals);
-  //     setLoading(false);
-  //   } else if (searchParam === 'first-letter' && termoBusca.length > 1) {
-  //     // eslint-disable-next-line no-alert
-  //     alert('Sua busca deve conter somente 1 (um) caracter');
-  //   }
-  // };
+  const buscaComidas = async () => {
+    if (searchParam === 'ingredient') {
+      setLoading(true);
+      const filteredMeals = await api.fetchFoodByIngredients(termoBusca);
+      setMeals(filteredMeals);
+      setLoading(false);
+    }
+    if (searchParam === 'name') {
+      setLoading(true);
+      const filteredMeals = await api.fetchFoodByName(termoBusca);
+      setMeals(filteredMeals);
+      setLoading(false);
+    }
+    if (termoBusca.length === 1 && searchParam === 'first-letter') {
+      setLoading(true);
+      const filteredMeals = await api.fetchFoodByFirstLetter(termoBusca);
+      setMeals(filteredMeals);
+      setLoading(false);
+    } else if (searchParam === 'first-letter' && termoBusca.length > 1) {
+      // eslint-disable-next-line no-alert
+      alert('Sua busca deve conter somente 1 (um) caracter');
+    }
+  };
 
-  // const buscaBebidas = async () => {
-  //   if (searchParam === 'ingredient') {
-  //     setLoading(true);
-  //     const filteredDrinks = await api.fetchDrinkByIngredients(termoBusca);
-  //     setDrinks(filteredDrinks);
-  //     setLoading(false);
-  //   }
-  //   if (searchParam === 'name') {
-  //     setLoading(true);
-  //     const filteredDrink = await api.fetchDrinkByName(termoBusca);
-  //     setDrinks(filteredDrink);
-  //     setLoading(false);
-  //   }
-  //   if (termoBusca.length === 1 && searchParam === 'first-letter') {
-  //     setLoading(true);
-  //     const filteredDrink = await api.fetchDrinkByFirstLetter(termoBusca);
-  //     setDrinks(filteredDrink);
-  //     setLoading(false);
-  //   } else if (searchParam === 'first-letter' && termoBusca.length > 1) {
-  //     // eslint-disable-next-line no-alert
-  //     alert('Sua busca deve conter somente 1 (um) caracter');
-  //   }
-  // };
+  const buscaBebidas = async () => {
+    if (searchParam === 'ingredient') {
+      setLoading(true);
+      const filteredDrinks = await api.fetchDrinkByIngredients(termoBusca);
+      setDrinks(filteredDrinks);
+      setLoading(false);
+    }
+    if (searchParam === 'name') {
+      setLoading(true);
+      const filteredDrink = await api.fetchDrinkByName(termoBusca);
+      setDrinks(filteredDrink);
+      setLoading(false);
+    }
+    if (termoBusca.length === 1 && searchParam === 'first-letter') {
+      setLoading(true);
+      const filteredDrink = await api.fetchDrinkByFirstLetter(termoBusca);
+      setDrinks(filteredDrink);
+      setLoading(false);
+    } else if (searchParam === 'first-letter' && termoBusca.length > 1) {
+      // eslint-disable-next-line no-alert
+      alert('Sua busca deve conter somente 1 (um) caracter');
+    }
+  };
 
-  // const onClick = () => {
-  //   if (titulo === 'Comidas') {
-  //     buscaComidas();
-  //   }
-  //   if (titulo === 'Bebidas') {
-  //     buscaBebidas();
-  //   }
-  // };
+  const onClick = () => {
+    if (titulo === 'Comidas') {
+      buscaComidas();
+    }
+    if (titulo === 'Bebidas') {
+      buscaBebidas();
+    }
+  };
 
   return (
     <div>
@@ -80,7 +82,7 @@ export default function SearchBar() {
         name="search"
         data-testid="ingredient-search-radio"
         value="ingredient"
-        // onChange={ handleChange }
+        onChange={ handleChange }
       />
       Ingrediente
       <input
@@ -88,7 +90,7 @@ export default function SearchBar() {
         name="search"
         data-testid="name-search-radio"
         value="name"
-        // onChange={ handleChange }
+        onChange={ handleChange }
       />
       Nome
       <input
@@ -96,16 +98,18 @@ export default function SearchBar() {
         name="search"
         data-testid="first-letter-search-radio"
         value="first-letter"
-        // onChange={ handleChange }
+        onChange={ handleChange }
       />
       Primeira letra
       <button
         type="button"
         data-testid="exec-search-btn"
-        // onClick={ onClick }
+        onClick={ onClick }
       >
         Busca
       </button>
     </div>
   );
 }
+
+export default SearchBar;
